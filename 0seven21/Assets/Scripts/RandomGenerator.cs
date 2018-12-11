a273fbc3de585dbfe491151dc4c52b163473c87a
UnkoTaberu <44854165+UnkoTaberu@users.noreply.github.com>
2018-12-11 12:12:05 +0900

    dekita

--- 0seven21/Assets/Scripts/RandomGenerator.cs
+++ 0seven21/Assets/Scripts/RandomGenerator.cs
@@ -6,86 +6,100 @@ public class RandomGenerator : MonoBehaviour {
 
     [SerializeField]
     DungeonGenerator generator;
+
     [SerializeField]
     GameObject item;
     [SerializeField]
     Transform ItemtileContainer;
-
-    public int[,] itemmap;
-    [Range(0, 20)]
-    public int item_capacity = 3;
 
     [SerializeField]
     GameObject steps;
     [SerializeField]
     Transform StepstileContainer;
 
-    public int[,] stepsmap;
+    public int[,] spmap;
+    [Range(0, 20)]
+    public int sp_capacity = 3;
     
 
-    public void ItemGenerate(int[,] map)
+    public void SpGenerate(int[,] map)
     {
         int x = 0, y = 0, i = 0;
         item.gameObject.SetActive(true);
 
-        itemmap = new int[generator.width, generator.height];
+        spmap = new int[generator.width, generator.height];
 
+        // マップを複製
         for (x = 0; x != generator.width; x++)
         {
             for (y = 0; y != generator.height; y++)
             {
-                itemmap[x, y] = map[x, y];
+                spmap[x, y] = map[x, y];
             }
         }
 
-        while (i != item_capacity)
+        // アイテム、階段を配置
+        while (i != sp_capacity + 1)
         {
             System.Random rx = new System.Random();
             System.Random ry = new System.Random(rx.Next());
-            if (itemmap[x = rx.Next(generator.width), y = ry.Next(generator.height)] == 1)
+
+            if (spmap[x = rx.Next(generator.width), y = ry.Next(generator.height)] == 1 && i != sp_capacity) // アイテムを配置
             {
-                itemmap[x, y] = 3;
+                spmap[x, y] = 3;
                 var tile = Instantiate(item);
                 tile.transform.SetParent(ItemtileContainer);
                 tile.transform.localPosition = new Vector2(x, y);
 
                 i++;
+
+            }else if(i == sp_capacity) //階段を配置
+            {
+                if (spmap[x = rx.Next(generator.width), y = ry.Next(generator.height)] == 1)
+                {
+                    spmap[x, y] = 2;
+                    var tile = Instantiate(steps);
+                    tile.transform.SetParent(StepstileContainer);
+                    tile.transform.localPosition = new Vector2(x, y);
+
+                    i++;
+                }
             }
 
         }
 
     }
 
-    public void StepsGenerate(int[,] map)
-    {
-        int x = 0, y = 0, i = 0;
-        steps.gameObject.SetActive(true);
+    //public void StepsGenerate(int[,] map)
+    //{
+    //    int x = 0, y = 0, i = 0;
+    //    steps.gameObject.SetActive(true);
 
-        stepsmap = new int[generator.width, generator.height];
+    //    stepsmap = new int[generator.width, generator.height];
 
-        for (x = 0; x != generator.width; x++)
-        {
-            for (y = 0; y != generator.height; y++)
-            {
-                stepsmap[x, y] = map[x, y];
-            }
-        }
+    //    for (x = 0; x != generator.width; x++)
+    //    {
+    //        for (y = 0; y != generator.height; y++)
+    //        {
+    //            stepsmap[x, y] = map[x, y];
+    //        }
+    //    }
 
-        while (i != 1)
-        {
-            System.Random rx = new System.Random();
-            System.Random ry = new System.Random();
-            if (itemmap[x = rx.Next(generator.width), y = ry.Next(generator.height)] == 1)
-            {
-                stepsmap[x, y] = 2;
-                var tile = Instantiate(steps);
-                tile.transform.SetParent(StepstileContainer);
-                tile.transform.localPosition = new Vector2(x, y);
+    //    while (i != 1)
+    //    {
+    //        System.Random rx = new System.Random();
+    //        System.Random ry = new System.Random();
+    //        if (itemmap[x = rx.Next(generator.width), y = ry.Next(generator.height)] == 1)
+    //        {
+    //            stepsmap[x, y] = 2;
+    //            var tile = Instantiate(steps);
+    //            tile.transform.SetParent(StepstileContainer);
+    //            tile.transform.localPosition = new Vector2(x, y);
 
-                i++;
-            }
+    //            i++;
+    //        }
 
-        }
-    }
+    //    }
+    //}
 
 }
