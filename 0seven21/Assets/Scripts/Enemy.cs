c9d52840b260692bd65e975675a4b23d6e37224e
UnkoTaberu <44854165+UnkoTaberu@users.noreply.github.com>
2019-01-17 11:31:21 +0900

    komi

--- 0seven21/Assets/Scripts/Enemy.cs
+++ 0seven21/Assets/Scripts/Enemy.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Enemy : MonoBehaviour
@@ -23,6 +24,10 @@ public class Enemy : MonoBehaviour
     //コストを保持
     int[] enemy_cost = new int[10];
 
+    //hpbar用
+    [SerializeField]
+    HpBarCtrl hbc;
+
     // Use this for initialization
     void Start()
     {
@@ -33,6 +38,10 @@ public class Enemy : MonoBehaviour
         mytransform = this.transform;
         GameObject sc = GameObject.Find("SceneController");
         scontroller = sc.gameObject.GetComponent<SceneController>();
+
+        //hpbar用
+        Slider slider = GameObject.Find("Slider").GetComponent<Slider>();
+        hbc = slider.gameObject.GetComponent<HpBarCtrl>();
     }
 
     // Update is called once per frame
@@ -47,31 +56,35 @@ public class Enemy : MonoBehaviour
         enemy_num = 0;
         max_x = 0;
         max_y = 0;
+        int count = 1;
 
         //移動できるか
         for (int xx = (int)e_tmp.x - 1; (int)e_tmp.x + 2 != xx; xx++)
         {
             for (int yy = (int)e_tmp.y - 1; (int)e_tmp.y + 2 != yy; yy++)
             {
-
-                //Debug.Log(xx + "  " + yy);
-                //壁じゃない　かつ　自身の位置　ではない
-                if (scontroller.map[xx, yy] != 0 && scontroller.map[xx, yy] != scontroller.map[(int)e_tmp.x, (int)e_tmp.y])
+                if (count % 2 == 0)
                 {
-
-                    //コスト計算
-                    enemy_cost[enemy_num] = Math.Abs(xx - (int)player_tmp.x) + Math.Abs(yy - (int)player_tmp.y);
-
-                    //一番低いコストを保持
-                    if (cost_max > enemy_cost[enemy_num])
+                    //Debug.Log(xx + "  " + yy);
+                    //壁じゃない　かつ　自身の位置　ではない
+                    if (scontroller.map[xx, yy] != 0 && scontroller.map[xx, yy] != scontroller.map[(int)e_tmp.x, (int)e_tmp.y])
                     {
-                        cost_max = enemy_cost[enemy_num];
 
-                        max_x = xx;
-                        max_y = yy;
+                        //コスト計算
+                        enemy_cost[enemy_num] = Math.Abs(xx - (int)player_tmp.x) + Math.Abs(yy - (int)player_tmp.y);
+
+                        //一番低いコストを保持
+                        if (cost_max > enemy_cost[enemy_num])
+                        {
+                            cost_max = enemy_cost[enemy_num];
+
+                            max_x = xx;
+                            max_y = yy;
+                        }
+                        enemy_num++;
                     }
-                    enemy_num++;
                 }
+                count++;
             }
         }
 
@@ -85,6 +98,10 @@ public class Enemy : MonoBehaviour
             EnemyWas(scontroller.map);
             mytransform.position = e_tmp;
             EnemyNow(scontroller.map);
+        }
+        else
+        {
+            hbc.HpDamage();
         }
 
     }
