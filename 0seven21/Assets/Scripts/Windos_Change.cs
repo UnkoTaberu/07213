37d2f11eb67a9f4c6d00e229f47e84fe19382fde
N133 <44888605+N133@users.noreply.github.com>
2019-01-21 09:52:48 +0900

    Windos_Change

---
+++ 0seven21/Assets/Scripts/Windos_Change.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Windos_Change : MonoBehaviour
+{
+
+    public int ScreenWidth;
+    public int ScreenHeight;
+
+    void Awake()
+    {
+        if (Application.platform == RuntimePlatform.WindowsPlayer ||
+        Application.platform == RuntimePlatform.OSXPlayer ||
+        Application.platform == RuntimePlatform.LinuxPlayer)
+        {
+            Screen.SetResolution(ScreenWidth, ScreenHeight, false);
+        }
+    }
+}
